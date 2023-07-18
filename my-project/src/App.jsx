69b5055b6bf55a4f31f543c09c86b1d4ebce3087
vyshnav <vyshnavravi599@gmail.com>
2023-07-18 21:34:36 +0530

    updated

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -8,19 +8,31 @@ import {createUserWithEmailAndPassword} from 'firebase/auth'
 export default function Login_page() {
   const [email,setEmail] = useState(null) 
   const [password,setPassword] = useState(null)
-  const [confirmPassword,setConfirmPassword] = useState(null)
-  const [login,setLogin] = useState(false)
+  const [error,setError] = useState ("")
+  const [login,setLogin] = useState()
+  const [userState,setUserState] =useState("")
   console.log(email)
 
 const signIn = async()=>{
   try {
     await createUserWithEmailAndPassword(auth,email,password)
+    setLogin(true)
+    if(setLogin){
+      setUserState("Account created successfully ")
+    }
   }
   catch(err) {
-    console.log(err)
+    setLogin(false)
+    setUserState(err.message)
   }
 
+  
 
+
+}
+
+const valueChange = (e)=>{
+  e.target.value = ''
 
 }
 
@@ -29,16 +41,22 @@ const signIn = async()=>{
     
     
     
-    <div className='flex justify-center flex-col items-center'>
+    <div className='flex justify-center flex-col items-center '>
     Email:<br/>
-   <input className='border-red-600 border-2 w-1/4' type='email' placeholder='Email' onChange={e => setEmail(e.target.value)}/>
+   <input className='border-red-600 border-2 w-1/4' type='email' placeholder='Email' onFocus={valueChange} onChange={e => setEmail(e.target.value)}/>
 
    Password:<br/>
-   <input className='border-red-600 border-2  w-1/4' type='password' placeholder='Password' onChange={e => setPassword(e.target.value)}/>
-   
+   <input className='border-red-600 border-2  w-1/4' type='password' placeholder='Password' onFocus={valueChange} onChange={e => setPassword(e.target.value)}/>
+
 
    <button onClick={signIn} className='w-20 border mt-10 text-xl p-2 hover:bg-black hover:text-white'>Login</button>
+
+   <div>
+   <h1 className='font-bold'>{userState}</h1>
    </div>
+   </div>
+
+   
    
    
 
