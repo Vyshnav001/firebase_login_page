df944920c0b98faa58d0d006693d6b874f01c114
vyshnav <vyshnavravi599@gmail.com>
2023-08-02 16:30:48 +0530

    updated

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -4,18 +4,45 @@ import './App.css'
 import Userpage from './components/Userpage'
 import { auth } from './config/firebase'
 import {createUserWithEmailAndPassword} from 'firebase/auth'
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useEffect } from 'react';
+
 
 export default function Login_page() {
   const [email,setEmail] = useState(null) 
   const [password,setPassword] = useState(null)
-  const [error,setError] = useState ("")
+  const [userDetails, setUserDetails] = useState ("")
   const [login,setLogin] = useState()
   const [userState,setUserState] =useState("")
   console.log(email)
 
+  const refresh = ()=>{
+    setUserDetails(null)
+    setUserState(null)
+    window.location.reload()
+
+ }
+
+  // const getuser = async()=>{
+  //   const auth = getAuth();
+  //   onAuthStateChanged(auth, (user) => {
+  //     if (user) {
+  //       setUserDetails(user)
+  //       console.log(userDetails)
+  //       setUserState(`User is signed in with ${user.email}`)
+  //     } else {
+  //       setUserState("User is signed out")
+  //     }
+  //   });
+  // }
+
+
 const signIn = async()=>{
+  
+  if (email != null && password != null){
   try {
     await createUserWithEmailAndPassword(auth,email,password)
+    console.log(auth)
     setLogin(true)
     if(setLogin){
       setUserState("Account created successfully ")
@@ -25,7 +52,10 @@ const signIn = async()=>{
     setLogin(false)
     setUserState(err.message)
   }
-
+}
+else {
+  setUserState("Please enter email and password")
+}
   
 
 
@@ -43,19 +73,22 @@ const valueChange = (e)=>{
     
     <div className='flex justify-center flex-col items-center '>
     Email:<br/>
-   <input className='border-red-600 border-2 w-1/4' type='email' placeholder='Email' onFocus={valueChange} onChange={e => setEmail(e.target.value)}/>
+   <input className='border-red-600 border-2 w-1/4' type='email' required = "required" placeholder='Email'   onChange={e => setEmail(e.target.value)}/>
 
    Password:<br/>
-   <input className='border-red-600 border-2  w-1/4' type='password' placeholder='Password' onFocus={valueChange} onChange={e => setPassword(e.target.value)}/>
+   <input className='border-red-600 border-2  w-1/4' type='password' required = "required" placeholder='Password'  onChange={e => setPassword(e.target.value)}/>
 
 
    <button onClick={signIn} className='w-20 border mt-10 text-xl p-2 hover:bg-black hover:text-white'>Login</button>
 
+   {/* <button onClick={getuser} className='w-20 border mt-10 text-xl p-2 hover:bg-black hover:text-white'>Get user details</button> */}
+   <button onClick={refresh} className='w-20 border mt-10 text-xl p-2 hover:bg-black hover:text-white'>Refresh</button>
    <div>
    <h1 className='font-bold'>{userState}</h1>
-   </div>
-   </div>
 
+   </div>
+   </div>
+  
    
    
    
